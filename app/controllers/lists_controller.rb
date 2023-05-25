2524c04c05828b5a5fa1caa574dccca898641e54
Marina Stepanova <stepanov.mari@gmail.com>
2023-05-25 15:01:59 +0200

    backenddddd

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,36 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    # @list = List.find(params[:list_id])
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = list.find(params[:id])
+    @list.destroy
+    redirect_to lists_url
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources: movies, only: [:show, :index:]
+
+  concern :bookmarkable do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  # resources :movies, concerns: :bookmarkable
+  resources :lists, concerns: :bookmarkable, only: [:new, :create, :show, :index]
+
+  resources :bookmarks, only: :destroy
 end
