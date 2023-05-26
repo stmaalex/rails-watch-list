13c1c048ec85300289ee9da66e2f38b967902cee
Marina Stepanova <stepanov.mari@gmail.com>
2023-05-26 17:09:10 +0200

    until I fhdjfhksdjf

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,5 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy #@list.movies
   validates :name, presence: true
   validates :name, uniqueness: true
-  has_one_attached :banner_photo
+  has_one_attached :photo
 end
