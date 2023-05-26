fd7791437c5210ef3a1931fb7bd4c259d6cd32c3
Marina Stepanova <stepanov.mari@gmail.com>
2023-05-26 16:25:49 +0200

    until I break it again

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,5 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy #@list.movies
   validates :name, presence: true
   validates :name, uniqueness: true
+  has_one_attached :banner_photo
 end
